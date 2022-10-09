c52e854481d0f1639774a2e0b6ab1a818d175f15
Szymon Cieśla <cszymon01@gmail.com>
2022-10-10 01:22:38 +0200

    finished project

--- script.js
+++ script.js
@@ -6,4 +6,53 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = '&appid=77733ffbadd91e60abedf5e9ff249529'
+const API_UNITS = '&units=metric'
 
+const getWeather = () => {
+	const city = input.value || 'Londyn'
+	const URL = API_LINK + city + API_KEY + API_UNITS
+	axios
+		.get(URL)
+		.then(res => {
+			console.log(res.data)
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			const status = Object.assign({}, ...res.data.weather)
+			weather.textContent = status.main
+			warning.textContent = ''
+			input.value = ''
+
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.floor(temp) + '℃'
+			humidity.textContent = hum + '%'
+			if (status.id >= 200 && status.id < 300) {
+				photo.setAttribute('src', './img/thunderstorm.png')
+			} else if (status.id >= 300 && status.id < 400) {
+				photo.setAttribute('src', './img/drizzle.png')
+			} else if (status.id >= 500 && status.id < 600) {
+				photo.setAttribute('src', './img/rain.png')
+			} else if (status.id >= 600 && status.id < 700) {
+				photo.setAttribute('src', './img/ice.png')
+			} else if (status.id >= 700 && status.id < 800) {
+				photo.setAttribute('src', './img/fog.png')
+			} else if (status.id === 800) {
+				photo.setAttribute('src', './img/sun.png')
+			} else if (status.id > 800 && status.id < 900) {
+				photo.setAttribute('src', './img/cloud.png')
+			} else {
+				photo.setAttribute('src', './img/unknown.png')
+			}
+		})
+		.catch(() => (warning.textContent = 'Wpisz poprawną nazwę'))
+}
+
+const enterCheck = e => {
+	if (e.key === 'Enter') {
+		getWeather()
+	}
+}
+input.addEventListener('keyup', enterCheck)
+getWeather()
+button.addEventListener('click', getWeather)
